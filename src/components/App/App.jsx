c689afe65286c79dd76083792ae452d04946ec82
saharoori <saharlaor@gmail.com>
2021-12-23 17:35:00 +0200

    Create main app component structure and methods

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import Counter from "../Counter/Counter";
+import Item from "../Item/Item";
+import Button from "../Button/Button";
+
+import LikeImg from "../../assets/img/like.png";
+import DislikeImg from "../../assets/img/dislike.png";
+
 import "./App.css";
 
 class App extends React.Component {
   state = { likes: 0, dislikes: 0 };
 
+  getImgIndex() {
+    return this.state.likes + this.state.dislikes;
+  }
+
+  addLike() {
+    this.setState((prevState) => {
+      likes: prevState.likes + 1;
+    });
+  }
+
+  addDislike() {
+    this.setState((prevState) => {
+      dislikes: prevState.dislikes + 1;
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <Counter img={LikeImg} text={this.state.likes} />
+        <Counter img={DislikeImg} text={this.state.dislikes} />
+        <Item img={this.getImgIndex()} />
+        <Button text="Like" handleClick={this.addLike} />
+        <Button text="Dislike" handleClick={this.addDislike} />
       </div>
     );
   }
